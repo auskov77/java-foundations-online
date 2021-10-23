ce9edd7548914120420c34f4134889671787330f
auskov77 <auskov77@yandex.ru>
2021-10-23 21:56:25 +0300

    Delete delombok/src-bak/src/main/java/ru/itsjava directory

--- delombok/src-bak/src/main/java/ru/itsjava/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.itsjava;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hola!");
-    }
-}
-
--- delombok/src-bak/src/main/java/ru/itsjava/Object_2/Object.java
+++
@@ -1,9 +0,0 @@
-package ru.itsjava.Object_2;
-
-public class Object {
-    public static void main(String[] args) {
-
-
-
-    }
-}
