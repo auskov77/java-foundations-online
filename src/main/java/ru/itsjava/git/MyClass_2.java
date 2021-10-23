dc8f42c66d9d91eb72d588119dbe18a21c0003c5
auskov77 <auskov77@yandex.ru>
2021-10-23 21:35:19 +0300

    Initial project version

--- src/main/java/ru/itsjava/git/MyClass_2.java
+++ src/main/java/ru/itsjava/git/MyClass_2.java
@@ -1,4 +1,33 @@
 package ru.itsjava.git;
 
 public class MyClass_2 {
+    public static void main(String[] args) {
+        int three = 3;
+        int seven = 7;
+
+//Задача 1
+        if (three > 5)
+            System.out.println("1");
+
+
+//Задача 2
+        if (seven > 5)
+        System.out.println("2");
+        System.out.println("3");
+
+//Задача 3
+        if (three > 5) {
+            System.out.println("4");
+        }
+
+//Задача 4
+        if (three > 5) {
+            System.out.println("5");
+        }
+
+//Задача 5
+        if (three > 5) ;
+        System.out.println("6");
+
+    }
 }
