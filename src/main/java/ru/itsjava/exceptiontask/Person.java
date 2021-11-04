c917876afb928a5ad3a84cdf5ee837e2ebb78da4
auskov77 <auskov77@yandex.ru>
2021-11-04 19:31:07 +0300

    exceptiontask

--- src/main/java/ru/itsjava/exceptiontask/Person.java
+++ src/main/java/ru/itsjava/exceptiontask/Person.java
@@ -11,12 +11,9 @@ public class Person {
 
     public int getAgeInInterval() {
         try {
-            return age;
+                return age;
         } catch (ArithmeticException arithmeticException) {
             throw new AgeNotValidException("неверный возраст");
         }
     }
 }
-//1. Создать класс Person c полями String name и int age.                       - yes
-//2. Создать свою ошибку AgeNotValidException                                   - yes
-//3. Бросать ошибку в классе Person , если возраст меньше нуля или больше 150   -
