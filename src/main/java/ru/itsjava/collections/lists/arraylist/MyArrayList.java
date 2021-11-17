3adf38b826d62f2a930dae80646ea256507dd4cd
auskov77 <auskov77@yandex.ru>
2021-11-17 19:47:18 +0300

    ArrayList_3

--- src/main/java/ru/itsjava/collections/lists/arraylist/MyArrayList.java
+++ src/main/java/ru/itsjava/collections/lists/arraylist/MyArrayList.java
@@ -1,9 +1,8 @@
 package ru.itsjava.collections.lists.arraylist;
 
-import java.util.Arrays;
-
 public class MyArrayList {
     private static final int DEFAULT_CAPACITY = 10;
+    private int capacity;
     private int realSize;
     private Object[] array;
 
@@ -43,8 +42,23 @@ public class MyArrayList {
         return true;
     }
 
-    public boolean remove(Object o) {
-        return false;
+    public boolean remove(Object o) {               // удаляем объект о из списка
+        if (o == null) {                            // проверка условия что объект о не пустой
+            for (int i = 0; i < realSize; i++) {    // цикл от 0 до realSize с шагом 1 - проверка элементов массива
+                if (array[i] == null){              // проверка условия что элемент массива с индексом i не пустой
+                    remove(i);                      // удаляем элемент с индексом i
+                    return true;                    // возвращаем истину
+                }
+            }
+        }else {                                     // иначе
+            for (int i = 0; i < realSize; i++) {    // цикл от 0 до realSize с шагом 1 - проверка элементов массива
+                if (o.equals(array[i])){            // проверка условия что объект о совпадает с элементом массива i
+                    remove(i);                      // удаляем элемент с индексом i
+                    return true;                    // возвращаем истину
+                }
+            }
+        }
+        return false;                               // возвращаем ложь
     }
 
     public void clear() { // используется для очистки коллекции, для которой он вызывается
@@ -54,28 +68,81 @@ public class MyArrayList {
         realSize = 0;
     }
 
-    public Object get(int index) {
-        return null;
+    public Object get(int index) { // получить элемент по указанному индексу
+        checkIndex(index);
+
+        return array[index];
     }
 
-    public Object set(int index, Object element) {
-        return null;
+    public Object set(int index, Object element) { // устанавить элемент в указанную позицию
+        checkIndex(index);
+
+        Object resElement = array[index];
+        array[index] = element;
+
+        return resElement;
     }
 
-    public void add(int index, Object element) {
+    public void add(int index, Object element) { // добавить элемент на указанную позицию
+        checkIndex(index);
 
+        if (realSize + 1 >= capacity) { //если в массиве места нет
+            increasingArrayCapacity(); //вызываем метод, который отвечает за увеличение массива
+        }
+        if (realSize + 1 - index >= 0) System.arraycopy(array, index - 1, array, index, realSize + 1 - index);
+        array[index] = element;
+        realSize++;
+    }
+
+    // метод для увеличения емкости массива
+    private void increasingArrayCapacity() {
+        capacity = realSize + 1;
+        Object[] newArray = new Object[capacity];
+        for (int i = 0; i < realSize; i++) {
+            newArray[i] = array[i];
+            array[i] = null;
+        }
+        array = newArray;
     }
 
     public Object remove(int index) {
-        return null;
+        checkIndex(index);
+
+        Object resElement = array[index];
+        if (array.length - 1 - index >= 0) {
+            System.arraycopy(array, index + 1, array, index, array.length - 1 - index);
+        }
+        realSize--;
+        return resElement;
     }
 
-    public int indexOf(Object o) {
-        return 0;
+    private void checkIndex(int index) {
+        if (!isCorrectIndex(index)) {
+            throw new ArrayIndexOutOfBoundsException("Некорректный индекс");
+        }
     }
 
-    public int lastIndexOf(Object o) {
-        return 0;
+    private boolean isCorrectIndex(int index) {
+        // проверяем что (index > -1) && (index < realSize)
+        return (index > -1) && (index < realSize);
+    }
+
+    public int indexOf(Object o) { // возвращает индекс первого вхождения указанного объекта o
+        for (int i = 0; i < realSize; i++) { // пробегаемся по списку от 0 до realSize с шагом 1
+            if (o.equals(array[i])) { // условие совпадения объекта о со значением элемента списка
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int lastIndexOf(Object o) { // возвращает индекс последнего вхождения указанного значения в строковый объект String, на котором он был вызван, или -1, если ничего не было найдено
+        for (int i = realSize; i >= 0; i--) {
+            if (o.equals(array[i])) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
--- src/main/java/ru/itsjava/collections/lists/arraylist/MyArrayListPractice.java
+++ src/main/java/ru/itsjava/collections/lists/arraylist/MyArrayListPractice.java
@@ -28,5 +28,75 @@ public class MyArrayListPractice {
         List.clear();
         System.out.println(List);
 
+        List.add("Привет");
+        List.add("Пока");
+        List.add("Пока");
+        List.add("Пока");
+        List.add("Пока");
+        List.add("Пока");
+        List.add("Пока");
+        List.add("Пока");
+        List.add("Пока");
+        List.add("Пока");
+        List.add("Пока");
+        System.out.println("List.size() = " + List.size());
+
+        System.out.println(List);
+
+        System.out.println("List.remove(\"Привет\") = " + List.remove("Привет"));
+
+        System.out.println("List.size() = " + List.size());
+        System.out.println(List);
+        System.out.println("List.remove(\"Пока\") = " + List.remove("Пока"));
+        System.out.println(List);
+
+        List.add("До_свидания");
+        System.out.println(List);
+        System.out.println(List.size());
+        List.remove(9);
+        System.out.println(List);
+        System.out.println(List.size());
+        List.remove(1);
+        System.out.println(List);
+
+        MyArrayList List2 = new MyArrayList();
+        List2.add("element_1");
+        List2.add("element_2");
+        List2.add("element_3");
+        List2.add("element_4");
+        System.out.println(List2);
+        System.out.println("List2.size() = " + List2.size());
+
+//        List2.remove(1);
+//        System.out.println(List2);
+//
+//        List2.remove(0);
+//        System.out.println(List2);
+
+//        List2.remove("element_1");
+//        System.out.println(List2);
+//        System.out.println("List2.size() = " + List2.size());
+//
+//        List2.remove("element_3");
+//        System.out.println(List2);
+//        System.out.println("List2.size() = " + List2.size());
+
+        System.out.println("List2.get(1) = " + List2.get(1));
+        System.out.println("List2.get(4) = " + List2.get(3));
+
+        List2.set(0, "e_1");
+        System.out.println(List2);
+
+        List2.add(1, "e_2");
+        System.out.println(List2);
+
+        List2.add(3, "e_3");
+        System.out.println(List2);
+
+        System.out.println("List2.indexOf(\"e_2\") = " + List2.indexOf("e_2"));
+        System.out.println("List2.indexOf(\"e_4\") = " + List2.indexOf("e_4"));
+
+        System.out.println(List);
+        System.out.println("List.lastIndexOf(\"Пока\") = " + List.lastIndexOf("Пока"));
     }
 }
