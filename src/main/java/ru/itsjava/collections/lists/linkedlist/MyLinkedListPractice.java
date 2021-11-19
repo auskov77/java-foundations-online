5d9fcf1ae72edf91f59bdc1617c311029ec1900b
auskov77 <auskov77@yandex.ru>
2021-11-20 00:37:12 +0300

    MyArrayList_version_5

--- src/main/java/ru/itsjava/collections/lists/linkedlist/MyLinkedListPractice.java
+++ src/main/java/ru/itsjava/collections/lists/linkedlist/MyLinkedListPractice.java
@@ -27,39 +27,43 @@ public class MyLinkedListPractice {
 //        List.remove(2);
 //        System.out.println("List = " + List);
 
-        List.add("Строка_3");
+        List.add("Строка_4");
         System.out.println(List);
 
         List.clear();
         System.out.println(List);
+        MyLinkedList List3 = new MyLinkedList();
+        System.out.println(List3);
+        System.out.println("List3.size() = " + List3.size());
+        System.out.println("List.size() = " + List.size());
 
-        List.add("Строка_1");
-        List.add("Строка_2");
-        List.add("Строка_3");
-        System.out.println(List);
+//        List.add("Строка_1");
+//        List.add("Строка_2");
+//        List.add("Строка_3");
+//        System.out.println(List);
 
-        System.out.println("List.get(0) = " + List.get(0));
-        System.out.println("List.get(1) = " + List.get(1));
-        System.out.println("List.get(2) = " + List.get(2));
-
-//        List.set(1, "Новая_строка");
+//        System.out.println("List.get(0) = " + List.get(0));
+//        System.out.println("List.get(1) = " + List.get(1));
+//        System.out.println("List.get(2) = " + List.get(2));
+//
+////        List.set(1, "Новая_строка");
+////        System.out.println(List);
+////
+////        List.remove("Строка_1");
+////        System.out.println(List);
+////        System.out.println("List.remove(\"Строка_4\") = " + List.remove("Строка_4"));
 //        System.out.println(List);
 //
-//        List.remove("Строка_1");
-//        System.out.println(List);
-//        System.out.println("List.remove(\"Строка_4\") = " + List.remove("Строка_4"));
-        System.out.println(List);
-
-        System.out.println("List.indexOf(\"Строка_1\") = " + List.indexOf("Строка_1"));
-        System.out.println("List.indexOf(\"Строка_1\") = " + List.indexOf("Строка_2"));
-        System.out.println("List.indexOf(\"Строка_1\") = " + List.indexOf("Строка_3"));
-        System.out.println("List.indexOf(\"Строка_1\") = " + List.indexOf("Строка_4"));
-
-        MyLinkedList List3 = new MyLinkedList();
-        List3.add("c_1");
-        List3.add("c_1");
-        List3.add("c_1");
-        System.out.println(List3);
-        System.out.println("List3.lastIndexOf(\"c_1\") = " + List3.lastIndexOf("c_1"));
+//        System.out.println("List.indexOf(\"Строка_1\") = " + List.indexOf("Строка_1"));
+//        System.out.println("List.indexOf(\"Строка_1\") = " + List.indexOf("Строка_2"));
+//        System.out.println("List.indexOf(\"Строка_1\") = " + List.indexOf("Строка_3"));
+//        System.out.println("List.indexOf(\"Строка_1\") = " + List.indexOf("Строка_4"));
+//
+//        MyLinkedList List3 = new MyLinkedList();
+//        List3.add("c_1");
+//        List3.add("c_1");
+//        List3.add("c_1");
+//        System.out.println(List3);
+//        System.out.println("List3.lastIndexOf(\"c_1\") = " + List3.lastIndexOf("c_1"));
     }
 }
