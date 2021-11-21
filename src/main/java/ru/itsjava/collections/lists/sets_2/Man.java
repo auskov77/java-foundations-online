c14c9ffa25b7235d30e6aeb6c63edc1709ddbb80
auskov77 <auskov77@yandex.ru>
2021-11-21 14:21:41 +0300

    sets_2 version 1

---
+++ src/main/java/ru/itsjava/collections/lists/sets_2/Man.java
@@ -0,0 +1,11 @@
+package ru.itsjava.collections.lists.sets_2;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Man {
+    private final String name;
+
+}
