be5725247ecea7c37f56fe2ba8453392dbfc4a58
auskov77 <auskov77@yandex.ru>
2021-11-22 14:45:05 +0300

    SerializationPractice

--- src/main/java/ru/itsjava/iostreams/SerializationPractice.java
+++ src/main/java/ru/itsjava/iostreams/SerializationPractice.java
@@ -15,11 +15,29 @@ public class SerializationPractice {
 //            exception.printStackTrace();
 //        }
 
+//        try (ObjectInputStream inputStream = new ObjectInputStream(
+//                new FileInputStream("src/main/resources/man.out"))) {
+//            Object obj = inputStream.readObject();
+//            Man man = (Man) obj;
+//            System.out.println("man = " + man);
+//        } catch (IOException exception) {
+//            exception.printStackTrace();
+//        }
+
+//        User user = new User("alex", "qwerty");
+//
+//        try (ObjectOutputStream outputStream = new ObjectOutputStream(
+//                new FileOutputStream("src/main/resources/user.out"))) {
+//            outputStream.writeObject(user);
+//        } catch (IOException exception) {
+//            exception.printStackTrace();
+//        }
+
         try (ObjectInputStream inputStream = new ObjectInputStream(
-                new FileInputStream("src/main/resources/man.out"))) {
+                new FileInputStream("src/main/resources/user.out"))) {
             Object obj = inputStream.readObject();
-            Man man = (Man) obj;
-            System.out.println("man = " + man);
+            User user = (User)  obj;
+            System.out.println("user = " + user);
         } catch (IOException exception) {
             exception.printStackTrace();
         }
@@ -28,3 +46,6 @@ public class SerializationPractice {
 
     }
 }
+
+//        1. Объект класса User (login, password) Проставить transient на password              - yes
+//        Сериализовать объект. После десериализовать объект и вывести поля login и password    -
