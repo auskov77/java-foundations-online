607a9fb22343cb329a34b68a27096a082139ca80
auskov77 <auskov77@yandex.ru>
2021-11-22 16:05:02 +0300

    PropertiesPractice

---
+++ src/main/java/ru/itsjava/iostreams/MyPropertiesPractice.java
@@ -0,0 +1,29 @@
+package ru.itsjava.iostreams;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class MyPropertiesPractice {
+
+    public static void main(String[] args) throws IOException {
+        File file = new File("src/main/resources/application.properties");
+
+        Properties props = new Properties();
+        props.load(new FileInputStream(file));
+
+        System.out.println("props.getProperty(\"key1\") = " + props.getProperty("key1"));
+
+        props.setProperty("key2", "value2");
+        System.out.println("props.getProperty(\"key2\") = " + props.getProperty("key2"));
+
+        ClassLoader classLoader = MyPropertiesPractice.class.getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream("application.properties");
+
+        Properties propsFromStream = new Properties();
+        propsFromStream.load(inputStream);
+        System.out.println("propsFromStream.getProperty(\"key1\") = " + propsFromStream.getProperty("key1"));
+    }
+}
