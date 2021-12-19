3f327b0b98929ed5d27da16198d631b270af216c
auskov77 <auskov77@yandex.ru>
2021-12-19 09:45:21 +0300

    jdbc version 3

---
+++ src/main/java/ru/itsjava/jdbc/task/daotask/UserDAO.java
@@ -0,0 +1,5 @@
+package ru.itsjava.jdbc.task.daotask;
+
+public interface UserDAO {
+    int getAgeByUserName(String name);
+}
---
+++ src/main/java/ru/itsjava/jdbc/task/daotask/UserJdbcImpl.java
@@ -0,0 +1,26 @@
+package ru.itsjava.jdbc.task.daotask;
+
+import java.sql.*;
+
+public class UserJdbcImpl implements UserDAO {
+    public static final String MY_DB_URL = "jdbc:MySql://localhost:3306/schema_users_messages?serverTimezone=UTC";
+    public static final String MY_DB_LOGIN = "root";
+    public static final String MY_DB_PASSWORD = "root";
+
+    @Override
+    public int getAgeByUserName(String name) {
+        try (Connection connection = DriverManager.getConnection(MY_DB_URL, MY_DB_LOGIN, MY_DB_PASSWORD);
+             PreparedStatement preparedStatement =
+                     connection.prepareStatement("select age from schema_users_messages.users where name = ?")
+        ) {
+            preparedStatement.setString(1, "vasyan");
+            ResultSet resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            return resultSet.getInt("age");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+}
